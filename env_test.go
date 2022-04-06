d16c3712e49ef09886bb604dc280233b4e80e5f3
Carl Johnson <me@carlmjohnson.net>
2022-04-06 15:54:25 -0400

    Test: Use be library

--- env_test.go
+++ env_test.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/carlmjohnson/be"
 	"github.com/carlmjohnson/flagext"
 )
 
@@ -29,18 +30,6 @@ func ExampleParseEnv() {
 	// b 3
 }
 
-func assert(t *testing.T, assertion bool, format string, args ...interface{}) {
-	t.Helper()
-	if !assertion {
-		t.Fatalf(format, args...)
-	}
-}
-
-func assertNil(t *testing.T, err error) {
-	t.Helper()
-	assert(t, err == nil, "want nil error; got: %v", err)
-}
-
 func TestParseEnv(t *testing.T) {
 	// Don't override
 	fs := flag.NewFlagSet("ExampleParseEnv", flag.ContinueOnError)
@@ -48,14 +37,14 @@ func TestParseEnv(t *testing.T) {
 	fs.SetOutput(&buf)
 	a := fs.Int("a", 0, "")
 	err := fs.Parse([]string{"-a", "1"})
-	assertNil(t, err)
-	assert(t, *a == 1, "expected a == 1; got %d", *a)
+	be.NilErr(t, err)
+	be.Equal(t, 1, *a)
 	// Does not override existing values
 	os.Setenv("TEST_ENV_A", "y")
 	err = flagext.ParseEnv(fs, "TEST_ENV")
-	assertNil(t, err)
+	be.NilErr(t, err)
 	output := buf.String()
-	assert(t, output == "", "expected no output; got %q", output)
+	be.Zero(t, output)
 
 	// Convert kebabs
 	fs = flag.NewFlagSet("ExampleParseEnv", flag.ContinueOnError)
@@ -64,10 +53,10 @@ func TestParseEnv(t *testing.T) {
 	kebab := fs.Int("a-b-c", 0, "")
 	os.Setenv("TEST_ENV_A_B_C", "1")
 	err = flagext.ParseEnv(fs, "TEST_ENV")
-	assertNil(t, err)
-	assert(t, *kebab == 1, "expected kebab == 1; got %d", *kebab)
+	be.NilErr(t, err)
+	be.Equal(t, 1, *kebab)
 	output = buf.String()
-	assert(t, output == "", "expected no output; got %q", output)
+	be.Zero(t, output)
 
 	// With error
 	fs = flag.NewFlagSet("ExampleParseEnv", flag.ContinueOnError)
@@ -75,12 +64,12 @@ func TestParseEnv(t *testing.T) {
 	fs.SetOutput(&buf)
 	b := fs.Int("b", 0, "")
 	err = fs.Parse(nil)
-	assertNil(t, err)
-	assert(t, *b == 0, "expected b == 0; got %d", *b)
+	be.NilErr(t, err)
+	be.Zero(t, *b)
 	os.Setenv("TEST_ENV_B", "y")
 	err = flagext.ParseEnv(fs, "TEST_ENV")
-	assert(t, err != nil, "expected err; got nil")
+	be.Nonzero(t, err)
 	output = buf.String()
 	expected := "invalid value \"y\" for flag -b: parse error\nUsage of ExampleParseEnv:\n  -b int\n    \t\n"
-	assert(t, output == expected, "expected usage message; got %q", output)
+	be.Equal(t, expected, output)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/carlmjohnson/flagext
 
-go 1.16
+go 1.18
+
+require github.com/carlmjohnson/be v0.22.3
